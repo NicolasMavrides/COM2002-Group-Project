97c6f2af079a663b3d0a6016c983deea0014a48a
Nicolas Mavrides <npmavrides1@sheffield.ac.uk>
2016-11-08 21:10:44 +0000

    Added event handler/action listener + updated index page

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ import javax.swing.JLabel;
 
 public class Main extends JFrame {
 	public static void main(String[] args) {
+		//Startup the main menu page.
 		IndexPage Startup = new IndexPage();
 		Startup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Startup.setVisible(true);
