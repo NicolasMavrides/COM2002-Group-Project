6548d1591d5d7fe3617c7133256494df9d0f89c1
Nicolas Mavrides <npmavrides1@sheffield.ac.uk>
2016-11-20 20:19:53 +0000

    added unsubscribe feature

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-/*
-import java.awt.FlowLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+//import java.awt.FlowLayout;
+//import javax.swing.JFrame;
+//import javax.swing.JLabel;
 
-public class Main extends JFrame {
-	public static void main(String[] args) {
+//public class Main extends JFrame {
+//	public static void main(String[] args) {
 		//Startup the main menu page.
-		IndexPage Startup = new IndexPage();
-		Startup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Startup.setVisible(true);
+//		IndexPage Startup = new IndexPage();
+//		Startup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		Startup.setVisible(true);
 		
-		RegisterPatient regPatient = new RegisterPatient();
-		regPatient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		regPatient.setVisible(true);
-	}
-}
-*/
+//		RegisterPatient regPatient = new RegisterPatient();
+//		regPatient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		regPatient.setVisible(true);
+//	}
+//}
