105c9336ad2bd722ddc9fa00bf3f699a8d850951
Nicolas Mavrides <npmavrides1@sheffield.ac.uk>
2016-11-20 17:44:30 +0000

    health care plan queries added

--- src/HealthCarePlanFrame.java
+++ src/HealthCarePlanFrame.java
@@ -2,12 +2,15 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class HealthCarePlanFrame extends JFrame {
@@ -20,6 +23,8 @@ public class HealthCarePlanFrame extends JFrame {
 	private JButton subOralHealth;
 	private JButton subDRepair;
 	private JButton submit;
+	private JComboBox planList;
+	private String[] healthCarePlans;
 	
 	public HealthCarePlanFrame() {
 		super("Subscribe Patient to Health Plan");
@@ -27,62 +32,62 @@ public class HealthCarePlanFrame extends JFrame {
 		setSize(400,150);
 		setResizable(false);
 	    setLocationRelativeTo(null);
+	    String[] healthCarePlans = { "Maintenance Plan", "Oral Health Plan", "Dental Repair Plan", "NHS Free Plan" };
 	    
 	    //construct buttons and their labels
 	    patientID = new JLabel("Patient ID: ");
 	    planDesc = new JLabel("Plan description: ");
 	    patientIDf = new JTextField();
 	    planDetails = new JTextField();
-		subNHS = new JButton("NHS Free Plan");
-		subMaintenance = new JButton("Maintenance Plan");
-		subOralHealth = new JButton("Oral Health Plan");
-		subDRepair = new JButton("Dental Repair Plan");
+	    planList = new JComboBox(healthCarePlans);
+	    planList.setSelectedIndex(1);
 		submit = new JButton("Submit");
 		
 		//add buttons
 		add(patientID);
 		add(patientIDf);
-		add(subNHS);
-		add(subMaintenance);
-		add(subOralHealth);
-		add(subDRepair);
 		add(planDesc);
 		add(planDetails);
 		planDetails.setEditable(false);
+		add(planList);
 		add(submit);
 		
 		//USED TO SHOW THE OPTIONS BEFORE SUBMITTING IN PLANDETAILS FIELD
 		class EventHandler implements ActionListener {
 			public void actionPerformed(ActionEvent event) {
-				if (event.getSource() == subNHS) {	
-					planDetails.setText("2 checks, 2 hygene visits, 6 repairs");
-					//Children/U18 - no-charge, 2chk, 2hyg, 6rep
-				} else if (event.getSource() == subMaintenance) {
+				if (event.getSource() == planList && planList.getSelectedItem() == "Maintenance Plan") {
 					planDetails.setText("2 checks, 2 hygene visits");
-					//£15 2chk, 2hyg 
-				} else if (event.getSource() == subOralHealth) {
+
+				} else if (event.getSource() == planList && planList.getSelectedItem() == "Oral Health Plan") {
 					planDetails.setText("2 checks, 4 hygene visits");
-					//£21 4hyg, 2chk 
-				} else if (event.getSource() == subDRepair) {
-					planDetails.setText("2 checks, 2 hygene visits, 2 repairs");
-					//£36 2chk, 2hyg, 2rep
+
+				} else if (event.getSource() == planList && planList.getSelectedItem() == "Dental Repair Plan") {
+					planDetails.setText("2 checks, 4 hygene visits, 2 repairs");
+
+				} else if (event.getSource() == planList && planList.getSelectedItem() == "NHS Free Plan") {
+					planDetails.setText("2 checks, 2 hygene visits, 6 repairs");
+
 				} else if (event.getSource() == submit) {
-					//take the patientID and option picked
-					System.out.println("submitted: " + patientIDf.getText() + ", " + planDetails.getText());
-				} else {
-					//nothing
+					HealthCareQueryProcessor execute = new HealthCareQueryProcessor();
+					execute.memberExists(Integer.parseInt(patientIDf.getText()));
+					try {
+						if (execute.planExists(Integer.parseInt(patientIDf.getText())))
+							JOptionPane.showMessageDialog(null, "This member is already subscribed to a Health Care Plan", null, JOptionPane.ERROR_MESSAGE);
+						else { 
+							execute.SubscribeHealthPlan(Integer.parseInt(patientIDf.getText()), planList.getSelectedItem().toString());
+						}
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		}
 		
 		//Declare new object for event handler
 		EventHandler eHandler = new EventHandler();
-		subNHS.addActionListener(eHandler);
-		subMaintenance.addActionListener(eHandler);
-		subOralHealth.addActionListener(eHandler);
-		subDRepair.addActionListener(eHandler);
 		submit.addActionListener(eHandler);
-		
+		planList.addActionListener(eHandler);
 		setVisible(true);
 	}
 	
@@ -94,11 +99,16 @@ public class HealthCarePlanFrame extends JFrame {
 		public HealthCareQueryProcessor() {
 			super();
 		}
-		public void SubscribeHealthPlan(String id, String title, String name, String surname, String dob, String phoneNum, int houseNum, 
-				String streetf, String postCode, String city) throws SQLException {
+		public void SubscribeHealthPlan(int id, String name) throws SQLException {
 		 Statement stmt = con.createStatement();
-			stmt.executeUpdate("INSERT INTO Patient () VALUES ()");
+			stmt.executeUpdate("INSERT INTO Healthcare_Plan (Patient_ID, Prepaid_Checkups, Prepaid_Hygiene, Prepaid_Repairs, Name) "
+					+ "VALUES ('"+id+"','0','0','0','"+name+"')");
+		}
+		public boolean planExists(int id) throws SQLException {
+			Statement stmt = con.createStatement();
+		    ResultSet res = stmt.executeQuery("SELECT COUNT(*) FROM Healthcare_Plan WHERE Patient_ID = "+id);
+		    res.next();
+		    return res.getInt(1)==1;
 		}
 	}
-	
 }
